8dd645cca6333cbaaee2aa6214dcf7c6f669b1e8
Puspanjali Puhan <125045123+PuspanjaliPuhan@users.noreply.github.com>
2023-12-07 19:35:55 +0530

    Add files via upload

---
+++ #day3 Simple calculator.py
@@ -0,0 +1,37 @@
+#day3 Simple calculator
+x=int(input('Enter first number='))
+y=int(input('Enter second number='))
+a=x+y
+b=x-y
+c=x*y
+d=x/y
+e=x//y
+f=x%y
+g=x^y
+print('The value of',x,'+',y,'is',a)
+print('The value of',x,'-',y,'is',b)
+print('The value of',x,'*',y,'is',c)
+print('The value of',x,'/',y,'is',d)
+print('The value is',x,'//',y,'is',e)
+print('The value of',x,'%',y,'is',f)
+print('The value of',x,'^',y,'is',g)
+
+# Using function 
+def add(x,y):
+    return x+y
+def substract(x,y):
+    return x-y
+def multiplication(x,y):
+    return x*y
+def division(x,y):
+    if x!=0:
+      return x/y
+    else:
+        print('It cannot be divived')
+
+print('Addition', add(x,y))
+print('Substraction',substract(x,y))
+print('Multiplication', multiplication(x,y))
+print('Division',division(x,y))
+
+    
