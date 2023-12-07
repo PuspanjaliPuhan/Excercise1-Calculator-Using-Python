2cdc3c69dbb2f26176c031ec7619583a0e41062d
Puspanjali Puhan <125045123+PuspanjaliPuhan@users.noreply.github.com>
2023-12-07 19:54:45 +0530

    Update #Simple calculator.py
    This calculator uses 'User defined' value to calcularte in 🐍.

--- #Simple calculator.py
+++ #Simple calculator.py
@@ -1,4 +1,5 @@
 # Simple calculator
+# This is User input for calculator
 x=int(input('Enter first number='))
 y=int(input('Enter second number='))
 a=x+y
@@ -16,22 +17,22 @@ print('The value is',x,'//',y,'is',e)
 print('The value of',x,'%',y,'is',f)
 print('The value of',x,'^',y,'is',g)
 
-# # Using function 
-# def add(x,y):
-#     return x+y
-# def substract(x,y):
-#     return x-y
-# def multiplication(x,y):
-#     return x*y
-# def division(x,y):
-#     if x!=0:
-#       return x/y
-#     else:
-#         print('It cannot be divived')
+# Using function 
+def add(x,y):
+    return x+y
+def substract(x,y):
+    return x-y
+def multiplication(x,y):
+    return x*y
+def division(x,y):
+    if x!=0:
+      return x/y
+    else:
+        print('It cannot be divived')
 
-# print('Addition', add(x,y))
-# print('Substraction',substract(x,y))
-# print('Multiplication', multiplication(x,y))
-# print('Division',division(x,y))
+print('Addition', add(x,y))
+print('Substraction',substract(x,y))
+print('Multiplication', multiplication(x,y))
+print('Division',division(x,y))
 
     
